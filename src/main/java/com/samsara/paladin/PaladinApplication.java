06665c68c1022938386c6e6854688f9907d04b37
Aleksandar Pejkovic <aleksandar.pejkovic@endava.com>
2023-01-20 16:40:44 +0100

    init commit

---
+++ src/main/java/com/samsara/paladin/PaladinApplication.java
@@ -0,0 +1,13 @@
+package com.samsara.paladin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaladinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaladinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samsara/paladin/PaladinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsara.paladin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaladinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
